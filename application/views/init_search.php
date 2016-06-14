11a39481866e2dc5ee984bcab2f020cfb4de8a2c
rgscherf <rgscherf@gmail.com>
2016-06-14 12:38:55 -0400

    static assets in place

---
+++ application/views/init_search.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Welcome to TWIT!</title>
+        <link rel="stylesheet" href="css/style.css">
+        <script type="text/javascript" src="bower_components/react/react.min.js"></script>
+        <script type="text/javascript" src="bower_components/react/react-dom.min.js"></script>
+        <script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>
+    </head>
+    <body>
+        <div id="app">
+            <input type=text class="mainSearch">
+        </div>
+    </body>
+</html>
