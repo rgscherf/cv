1c7413011c6f27f6cb9adc3c8153f6fbfd159809
rgscherf <rgscherf@gmail.com>
2016-06-15 22:37:46 -0400

    ui mocks

--- public/js/twit.js
+++ public/js/twit.js
@@ -1,14 +1,90 @@
+// PageContainer has 3 children
+// SearchBox gets user requests, pipes them back to PageContainer
+// Sidebar displays info for current user
+// Timeline displays most recent commits for user
+
+// Data flow:
+//
+
 var SearchBox = React.createClass({
     displayName: "SearchBox",
+
+    getInitialState: function () {
+        return { search: "" };
+    },
+    keyDown: function (e) {
+        if (e.key === 'Enter') {
+            var s = e.target.value;
+            this.setState({ search: s });
+        }
+    },
+    render: function () {
+        return React.createElement(
+            "div",
+            null,
+            React.createElement(
+                "div",
+                { className: "searchBox" },
+                React.createElement(
+                    "div",
+                    { className: "logo" },
+                    "Twit"
+                ),
+                React.createElement("input", { type: "text",
+                    placeholder: "Find a github user name",
+                    className: "mainSearch",
+                    onKeyDown: this.keyDown })
+            ),
+            React.createElement(ContentContainer, null)
+        );
+    }
+
+});
+
+var ContentContainer = React.createClass({
+    displayName: "ContentContainer",
 
     render: function () {
         return React.createElement(
             "div",
-            { className: "searchBox" },
-            React.createElement("input", { type: "text", placeholder: "Search here!", className: "mainSearch" })
+            { className: "contentContainer" },
+            React.createElement(Sidebar, null),
+            React.createElement(Timeline, null)
+        );
+    }
+});
+
+var Sidebar = React.createClass({
+    displayName: "Sidebar",
+
+    render: function () {
+        return React.createElement(
+            "div",
+            { className: "sideBar" },
+            React.createElement(
+                "p",
+                null,
+                "Hello, I am here in the Sidebar this evening."
+            )
         );
     }
 
+});
+
+var Timeline = React.createClass({
+    displayName: "Timeline",
+
+    render: function () {
+        return React.createElement(
+            "div",
+            { className: "timeline" },
+            React.createElement(
+                "p",
+                null,
+                "This is your pilot speaking, from the timeline view..."
+            )
+        );
+    }
 });
 
 ReactDOM.render(React.createElement(SearchBox, null), document.getElementById("app"));
--- public/src/twit.js
+++ public/src/twit.js
@@ -1,12 +1,68 @@
+// PageContainer has 3 children
+// SearchBox gets user requests, pipes them back to PageContainer
+// Sidebar displays info for current user
+// Timeline displays most recent commits for user
+
+// Data flow:
+// 
+
 var SearchBox = React.createClass({
+    getInitialState: function() {
+        return {search: ""};
+    },
+    keyDown: function(e) {
+        if (e.key === 'Enter') {
+            var s = e.target.value;
+            this.setState({search: s});
+        }
+    },
     render: function() {
         return (
-            <div className="searchBox">
-                <input type="text" placeholder="Search here!" className="mainSearch" />
+            <div>
+                <div className="searchBox">
+                    <div className="logo">Twit</div>
+                    <input type="text" 
+                           placeholder="Find a github user name" 
+                           className="mainSearch" 
+                           onKeyDown={this.keyDown}/>
+                </div>
+                <ContentContainer />
             </div>
         );
     }
 
+});
+
+var ContentContainer = React.createClass({
+    render: function() {
+        return (
+            <div className="contentContainer">
+                <Sidebar />
+                <Timeline />
+            </div>
+        );
+    }
+});
+
+var Sidebar = React.createClass({
+    render: function() {
+        return (
+            <div className="sideBar">
+                <p>Hello, I am here in the Sidebar this evening.</p>
+            </div>
+        );
+    }
+
+});
+
+var Timeline = React.createClass({
+    render: function() {
+        return (
+            <div className="timeline">
+                <p>This is your pilot speaking, from the timeline view...</p>
+            </div>
+        );
+    }
 });
 
 ReactDOM.render(
