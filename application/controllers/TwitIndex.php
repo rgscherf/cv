b52ed9adc425ad0484b44fd4238b6daf336a4332
rgscherf <rgscherf@gmail.com>
2016-06-16 22:53:02 -0400

    app is done, as long as you don't click any links

--- application/controllers/TwitIndex.php
+++ application/controllers/TwitIndex.php
@@ -7,30 +7,53 @@ class TwitIndex extends CI_Controller {
         $this->load->view('init_search');
     }
 
+    private function githubRequest($url) {
+        // function calls do not work, whyyyy
+        $client = new GuzzleHttp\Client();
+        $req = $client->request('GET', $url);
+        $res = $req->getBody();
+        $m = json_decode($res, true);
+        return $m;
+    }
+
     public function getUser($search) {
         // http://docs.guzzlephp.org/en/latest/
         // GuzzleHTTP is great
 
+        // get user data and initialize return array
         $url = 'https://api.github.com/users/'.$search;
         $client = new GuzzleHttp\Client();
-        
+
         $req = $client->request('GET', $url);
-        $res = $req->getBody();
+        $res = $req->getbody();
         $map = json_decode($res, true);
         $val = ["avatar_url" => $map["avatar_url"],
                 "html_url" => $map["html_url"],
                 "login" => $map["login"],
                 "name" => $map["name"],
                 "public_repos" => $map["public_repos"]];
+
+        // now let's work on user events
+        $url = 'https://api.github.com/users/'.$search.'/events';
+        $client = new GuzzleHttp\Client();
+        $req = $client->request('GET', $url);
+        $res = $req->getbody();
+        $map = json_decode($res, true);
+        $map = array_filter($map, function($v) { return $v['type'] === 'PushEvent';} );
+        $commits = array_map( function($v) {
+            $ret = [
+                "created_at" => $v['created_at'],
+                "repo_name" => $v['repo']['name'],
+                "repo_url" => $v['repo']['url'],
+                "commit_message" => $v['payload']['commits'][0]['message'],
+                "commit_url" => $v['payload']['commits'][0]['url'],
+            ];
+            return $ret;
+        }, $map);
+        $val['commits'] = $commits;
         echo json_encode($val);
 
-
-        // $dec = json_decode($res);
-        // $d = ["name" => $dec->login];
-        // echo json_encode($d);
-
-        // return did not work,
-        // but echoing result did...
-        // echo $res;
+        // echo json_encode($val);
     }
+
 }
--- public/js/twit.js
+++ public/js/twit.js
@@ -13,11 +13,125 @@ var SearchBox = React.createClass({
         return {
             error: false,
             user: {
-                avatar_url: "https://avatars.githubusercontent.com/u/8053315?v=3",
-                html_url: "https://github.com/rgscherf",
-                login: "rgscherf",
-                name: "Rob Scherf",
-                public_repos: 14
+                "avatar_url": "https://avatars.githubusercontent.com/u/8053315?v=3",
+                "html_url": "https://github.com/rgscherf",
+                "login": "rgscherf",
+                "name": "Rob Scherf",
+                "public_repos": 14,
+                "commits": {
+                    "0": {
+                        "created_at": "2016-06-17T01:11:48Z",
+                        "repo_name": "rgscherf/twit",
+                        "repo_url": "https://api.github.com/repos/rgscherf/twit",
+                        "commit_message": "error pane for bad search",
+                        "commit_url": "https://api.github.com/repos/rgscherf/twit/commits/587881af3f175267b39bbe9066156a3a535600f3"
+                    },
+                    "1": {
+                        "created_at": "2016-06-17T00:41:59Z",
+                        "repo_name": "rgscherf/twit",
+                        "repo_url": "https://api.github.com/repos/rgscherf/twit",
+                        "commit_message": "sidebar link coloring",
+                        "commit_url": "https://api.github.com/repos/rgscherf/twit/commits/e3a41aa294088e7b26341aa53f3a6d0e780063c8"
+                    },
+                    "2": {
+                        "created_at": "2016-06-17T00:31:21Z",
+                        "repo_name": "rgscherf/twit",
+                        "repo_url": "https://api.github.com/repos/rgscherf/twit",
+                        "commit_message": "correct ajax target for user req",
+                        "commit_url": "https://api.github.com/repos/rgscherf/twit/commits/acf6d7ed2d07cc4cace5ed0cc8947c99acd3cc66"
+                    },
+                    "3": {
+                        "created_at": "2016-06-17T00:26:33Z",
+                        "repo_name": "rgscherf/twit",
+                        "repo_url": "https://api.github.com/repos/rgscherf/twit",
+                        "commit_message": "layout for sidebar",
+                        "commit_url": "https://api.github.com/repos/rgscherf/twit/commits/d91ffe7588073d2215e75f3c3bc036dbf936d0bd"
+                    },
+                    "4": {
+                        "created_at": "2016-06-16T17:02:07Z",
+                        "repo_name": "rgscherf/twit",
+                        "repo_url": "https://api.github.com/repos/rgscherf/twit",
+                        "commit_message": "ajax'in",
+                        "commit_url": "https://api.github.com/repos/rgscherf/twit/commits/67efa4fd55f9c2beddf4029ac104c08b55b49ea1"
+                    },
+                    "5": {
+                        "created_at": "2016-06-16T02:37:53Z",
+                        "repo_name": "rgscherf/twit",
+                        "repo_url": "https://api.github.com/repos/rgscherf/twit",
+                        "commit_message": "ui mocks",
+                        "commit_url": "https://api.github.com/repos/rgscherf/twit/commits/1c7413011c6f27f6cb9adc3c8153f6fbfd159809"
+                    },
+                    "7": {
+                        "created_at": "2016-06-15T16:57:12Z",
+                        "repo_name": "rgscherf/twit",
+                        "repo_url": "https://api.github.com/repos/rgscherf/twit",
+                        "commit_message": "static assets paths",
+                        "commit_url": "https://api.github.com/repos/rgscherf/twit/commits/606dd6dc3ddf65920d2d7200e7caaf62f6831db3"
+                    },
+                    "8": {
+                        "created_at": "2016-06-14T17:02:12Z",
+                        "repo_name": "rgscherf/twit",
+                        "repo_url": "https://api.github.com/repos/rgscherf/twit",
+                        "commit_message": "config links",
+                        "commit_url": "https://api.github.com/repos/rgscherf/twit/commits/0203e0aa9efa57e9c77a05ffb4f89f00ddec1bac"
+                    },
+                    "9": {
+                        "created_at": "2016-06-14T16:54:47Z",
+                        "repo_name": "rgscherf/twit",
+                        "repo_url": "https://api.github.com/repos/rgscherf/twit",
+                        "commit_message": "readme",
+                        "commit_url": "https://api.github.com/repos/rgscherf/twit/commits/c92aa2ce148934a599984f121f38733d5249411b"
+                    },
+                    "10": {
+                        "created_at": "2016-06-14T16:39:13Z",
+                        "repo_name": "rgscherf/twit",
+                        "repo_url": "https://api.github.com/repos/rgscherf/twit",
+                        "commit_message": "static assets in place",
+                        "commit_url": "https://api.github.com/repos/rgscherf/twit/commits/11a39481866e2dc5ee984bcab2f020cfb4de8a2c"
+                    },
+                    "11": {
+                        "created_at": "2016-06-14T16:13:18Z",
+                        "repo_name": "rgscherf/twit",
+                        "repo_url": "https://api.github.com/repos/rgscherf/twit",
+                        "commit_message": "initial commit",
+                        "commit_url": "https://api.github.com/repos/rgscherf/twit/commits/03ef2ed49215eee96afbebd8b0b7bfda7995f820"
+                    },
+                    "17": {
+                        "created_at": "2016-06-05T19:59:46Z",
+                        "repo_name": "rgscherf/i-am-a-horse-in-the-land-of-booleans",
+                        "repo_url": "https://api.github.com/repos/rgscherf/i-am-a-horse-in-the-land-of-booleans",
+                        "commit_message": "final",
+                        "commit_url": "https://api.github.com/repos/rgscherf/i-am-a-horse-in-the-land-of-booleans/commits/0c70a78b24a9351541991eaceff1fa9ce01972c1"
+                    },
+                    "22": {
+                        "created_at": "2016-06-05T19:29:35Z",
+                        "repo_name": "rgscherf/training-day",
+                        "repo_url": "https://api.github.com/repos/rgscherf/training-day",
+                        "commit_message": "done",
+                        "commit_url": "https://api.github.com/repos/rgscherf/training-day/commits/b9f5e67530ec25178427c2e58f088165db8e2e60"
+                    },
+                    "25": {
+                        "created_at": "2016-05-29T20:15:21Z",
+                        "repo_name": "rgscherf/spiritlord",
+                        "repo_url": "https://api.github.com/repos/rgscherf/spiritlord",
+                        "commit_message": "rat attack",
+                        "commit_url": "https://api.github.com/repos/rgscherf/spiritlord/commits/5d2eb0391ec6fa27a4d177805f74d272dc781698"
+                    },
+                    "26": {
+                        "created_at": "2016-05-28T18:33:16Z",
+                        "repo_name": "rgscherf/spiritlord",
+                        "repo_url": "https://api.github.com/repos/rgscherf/spiritlord",
+                        "commit_message": "figuring out mermaid secondary",
+                        "commit_url": "https://api.github.com/repos/rgscherf/spiritlord/commits/9b82c03418855e174818bca8ce01e66b7e2482e5"
+                    },
+                    "29": {
+                        "created_at": "2016-05-28T03:33:40Z",
+                        "repo_name": "rgscherf/gainful2",
+                        "repo_url": "https://api.github.com/repos/rgscherf/gainful2",
+                        "commit_message": "compile new wording",
+                        "commit_url": "https://api.github.com/repos/rgscherf/gainful2/commits/0c1c0b57f8aa7e609053e2caa4b9de82cde9e7e5"
+                    }
+                }
             }
         };
     },
@@ -40,6 +154,7 @@ var SearchBox = React.createClass({
                 this.setState({ user: data, error: false });
             }.bind(this),
             error: function (xhr, status, err) {
+                console.log(xhr);
                 this.setState({ error: true });
             }.bind(this)
         });
@@ -76,7 +191,7 @@ var SearchBox = React.createClass({
                     "div",
                     { className: "searchContainer" },
                     React.createElement("input", { type: "text",
-                        placeholder: "Find a github user to twit",
+                        placeholder: "Find github user to twit",
                         className: "mainSearch",
                         onKeyDown: this.keyDown }),
                     React.createElement(
@@ -100,7 +215,7 @@ var ContentContainer = React.createClass({
             "div",
             { className: "contentContainer" },
             React.createElement(Sidebar, { user: this.props.user }),
-            React.createElement(Timeline, null)
+            React.createElement(Timeline, { commits: this.props.user.commits })
         );
     }
 });
@@ -146,14 +261,42 @@ var Timeline = React.createClass({
     displayName: "Timeline",
 
     render: function () {
+        var a = $.map(this.props.commits, function (e) {
+            return e;
+        });
+        console.log(a);
+        var c = $.map(a, function (elem) {
+            return React.createElement(
+                "div",
+                { className: "twitCard shadow" },
+                React.createElement(
+                    "div",
+                    { className: "twitCardHeadline" },
+                    "Commit to ",
+                    React.createElement(
+                        "a",
+                        { href: elem.repo_url },
+                        elem.repo_name
+                    ),
+                    " at ",
+                    React.createElement(
+                        "a",
+                        { href: elem.commit_url },
+                        elem.created_at
+                    )
+                ),
+                React.createElement(
+                    "div",
+                    { className: "twitCardBody" },
+                    elem.commit_message
+                )
+            );
+        });
+        console.log(c);
         return React.createElement(
             "div",
             { className: "timeline" },
-            React.createElement(
-                "p",
-                null,
-                "This is your pilot speaking, from the timeline view..."
-            )
+            c
         );
     }
 });
