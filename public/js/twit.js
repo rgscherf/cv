6da9c8f2188d93a39151a6b7265e3b0fdc1e9dc5
rgscherf <rgscherf@gmail.com>
2016-06-17 12:14:47 -0400

    mount with live data in SearchBox

--- public/js/twit.js
+++ public/js/twit.js
@@ -18,120 +18,7 @@ var SearchBox = React.createClass({
                 "login": "rgscherf",
                 "name": "Rob Scherf",
                 "public_repos": 14,
-                "commits": {
-                    "0": {
-                        "created_at": "2016-06-17T01:11:48Z",
-                        "repo_name": "rgscherf/twit",
-                        "repo_url": "https://api.github.com/repos/rgscherf/twit",
-                        "commit_message": "error pane for bad search",
-                        "commit_url": "https://api.github.com/repos/rgscherf/twit/commits/587881af3f175267b39bbe9066156a3a535600f3"
-                    },
-                    "1": {
-                        "created_at": "2016-06-17T00:41:59Z",
-                        "repo_name": "rgscherf/twit",
-                        "repo_url": "https://api.github.com/repos/rgscherf/twit",
-                        "commit_message": "sidebar link coloring",
-                        "commit_url": "https://api.github.com/repos/rgscherf/twit/commits/e3a41aa294088e7b26341aa53f3a6d0e780063c8"
-                    },
-                    "2": {
-                        "created_at": "2016-06-17T00:31:21Z",
-                        "repo_name": "rgscherf/twit",
-                        "repo_url": "https://api.github.com/repos/rgscherf/twit",
-                        "commit_message": "correct ajax target for user req",
-                        "commit_url": "https://api.github.com/repos/rgscherf/twit/commits/acf6d7ed2d07cc4cace5ed0cc8947c99acd3cc66"
-                    },
-                    "3": {
-                        "created_at": "2016-06-17T00:26:33Z",
-                        "repo_name": "rgscherf/twit",
-                        "repo_url": "https://api.github.com/repos/rgscherf/twit",
-                        "commit_message": "layout for sidebar",
-                        "commit_url": "https://api.github.com/repos/rgscherf/twit/commits/d91ffe7588073d2215e75f3c3bc036dbf936d0bd"
-                    },
-                    "4": {
-                        "created_at": "2016-06-16T17:02:07Z",
-                        "repo_name": "rgscherf/twit",
-                        "repo_url": "https://api.github.com/repos/rgscherf/twit",
-                        "commit_message": "ajax'in",
-                        "commit_url": "https://api.github.com/repos/rgscherf/twit/commits/67efa4fd55f9c2beddf4029ac104c08b55b49ea1"
-                    },
-                    "5": {
-                        "created_at": "2016-06-16T02:37:53Z",
-                        "repo_name": "rgscherf/twit",
-                        "repo_url": "https://api.github.com/repos/rgscherf/twit",
-                        "commit_message": "ui mocks",
-                        "commit_url": "https://api.github.com/repos/rgscherf/twit/commits/1c7413011c6f27f6cb9adc3c8153f6fbfd159809"
-                    },
-                    "7": {
-                        "created_at": "2016-06-15T16:57:12Z",
-                        "repo_name": "rgscherf/twit",
-                        "repo_url": "https://api.github.com/repos/rgscherf/twit",
-                        "commit_message": "static assets paths",
-                        "commit_url": "https://api.github.com/repos/rgscherf/twit/commits/606dd6dc3ddf65920d2d7200e7caaf62f6831db3"
-                    },
-                    "8": {
-                        "created_at": "2016-06-14T17:02:12Z",
-                        "repo_name": "rgscherf/twit",
-                        "repo_url": "https://api.github.com/repos/rgscherf/twit",
-                        "commit_message": "config links",
-                        "commit_url": "https://api.github.com/repos/rgscherf/twit/commits/0203e0aa9efa57e9c77a05ffb4f89f00ddec1bac"
-                    },
-                    "9": {
-                        "created_at": "2016-06-14T16:54:47Z",
-                        "repo_name": "rgscherf/twit",
-                        "repo_url": "https://api.github.com/repos/rgscherf/twit",
-                        "commit_message": "readme",
-                        "commit_url": "https://api.github.com/repos/rgscherf/twit/commits/c92aa2ce148934a599984f121f38733d5249411b"
-                    },
-                    "10": {
-                        "created_at": "2016-06-14T16:39:13Z",
-                        "repo_name": "rgscherf/twit",
-                        "repo_url": "https://api.github.com/repos/rgscherf/twit",
-                        "commit_message": "static assets in place",
-                        "commit_url": "https://api.github.com/repos/rgscherf/twit/commits/11a39481866e2dc5ee984bcab2f020cfb4de8a2c"
-                    },
-                    "11": {
-                        "created_at": "2016-06-14T16:13:18Z",
-                        "repo_name": "rgscherf/twit",
-                        "repo_url": "https://api.github.com/repos/rgscherf/twit",
-                        "commit_message": "initial commit",
-                        "commit_url": "https://api.github.com/repos/rgscherf/twit/commits/03ef2ed49215eee96afbebd8b0b7bfda7995f820"
-                    },
-                    "17": {
-                        "created_at": "2016-06-05T19:59:46Z",
-                        "repo_name": "rgscherf/i-am-a-horse-in-the-land-of-booleans",
-                        "repo_url": "https://api.github.com/repos/rgscherf/i-am-a-horse-in-the-land-of-booleans",
-                        "commit_message": "final",
-                        "commit_url": "https://api.github.com/repos/rgscherf/i-am-a-horse-in-the-land-of-booleans/commits/0c70a78b24a9351541991eaceff1fa9ce01972c1"
-                    },
-                    "22": {
-                        "created_at": "2016-06-05T19:29:35Z",
-                        "repo_name": "rgscherf/training-day",
-                        "repo_url": "https://api.github.com/repos/rgscherf/training-day",
-                        "commit_message": "done",
-                        "commit_url": "https://api.github.com/repos/rgscherf/training-day/commits/b9f5e67530ec25178427c2e58f088165db8e2e60"
-                    },
-                    "25": {
-                        "created_at": "2016-05-29T20:15:21Z",
-                        "repo_name": "rgscherf/spiritlord",
-                        "repo_url": "https://api.github.com/repos/rgscherf/spiritlord",
-                        "commit_message": "rat attack",
-                        "commit_url": "https://api.github.com/repos/rgscherf/spiritlord/commits/5d2eb0391ec6fa27a4d177805f74d272dc781698"
-                    },
-                    "26": {
-                        "created_at": "2016-05-28T18:33:16Z",
-                        "repo_name": "rgscherf/spiritlord",
-                        "repo_url": "https://api.github.com/repos/rgscherf/spiritlord",
-                        "commit_message": "figuring out mermaid secondary",
-                        "commit_url": "https://api.github.com/repos/rgscherf/spiritlord/commits/9b82c03418855e174818bca8ce01e66b7e2482e5"
-                    },
-                    "29": {
-                        "created_at": "2016-05-28T03:33:40Z",
-                        "repo_name": "rgscherf/gainful2",
-                        "repo_url": "https://api.github.com/repos/rgscherf/gainful2",
-                        "commit_message": "compile new wording",
-                        "commit_url": "https://api.github.com/repos/rgscherf/gainful2/commits/0c1c0b57f8aa7e609053e2caa4b9de82cde9e7e5"
-                    }
-                }
+                "commits": {}
             }
         };
     },
@@ -140,6 +27,9 @@ var SearchBox = React.createClass({
             var s = e.target.value;
             this.loadCommentsFromServer(s);
         }
+    },
+    componentWillMount: function () {
+        this.loadCommentsFromServer('rgscherf');
     },
     loadCommentsFromServer: function (query) {
         // var u = 'http://localhost:5000/index.php/twit/' + query;
