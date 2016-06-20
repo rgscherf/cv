fcbbc62f548c4fd912a900def7d80702425bc801
rgscherf <rgscherf@gmail.com>
2016-06-20 14:16:45 -0400

    basic css for job page

---
+++ application/views/home.php
@@ -0,0 +1,60 @@
+<html>
+    <head>
+        <title>@rgschef cv</title>
+        <link rel="stylesheet" href="../css/style.css">
+        <script type="text/javascript" src="../bower_components/jquery/dist/jquery.min.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Arvo|Codystar|Lato|Monofett|Patua+One|Poiret+One|Simonetta" rel="stylesheet">
+    </head>
+    <body>
+        <div id="headline">
+            Robert Scherf
+        </div>
+        <div id="subline">
+            Software developer
+        </div>
+        <div id="experience">
+            <div class="sectionHeader">
+                Education & Experience
+            </div>
+            <div class="sectionBody">
+                askghak;gha;k dghsdg
+                dfklgjhsdklfgh;dsfhg <br />
+                askghak;gha;k dghsdg
+                dfklgjhsdklfgh;dsfhg <br />
+                askghak;gha;k dghsdg
+                dfklgjhsdklfgh;dsfhg <br />
+            </div>
+        </div>
+        <div id=skills>
+            <div class="sectionHeader">
+                Skillset
+            </div>
+            <div class="sectionBody">
+                <div id="skillsHard">
+                    Python * * * * * <br />
+                    JavaScript * * * * <br />
+                    HTML5 & CSS * * * * <br />
+                    Elm * * * * <br />
+                    C# * * * <br />
+                    Java * * * <br />
+                    Clojure * * *  <br />
+                </div>
+                <div id="skillsSoft">
+                    Communications * * * * *  <br />
+                    Writing * * * * * <br />
+                    Teamwork * * * * *  <br />
+                    Leadership * * * * *  <br />
+                    Project Management * * * * * <br />
+                    Research * * * * * <br />
+                    Negotiation * * * * * <br />
+                </div>
+            </div>
+        </div>
+        <div id="projects">
+        </div>
+        <div id="commits">
+        </div>
+        <div id="contact">
+        </div>
+    </body>
+</html>
--- domains.md
+++ domains.md
@@ -1,12 +1,45 @@
-scherf.surf
-scherf.io
-scherf.lol
-scherf.life
-scherf.it
-scherf.zone
+## Domain ideas
 
-scherf.email
-scherf.digital
-scherf.download
-scherf.fail
-scherf.us
+- scherf.surf
+- scherf.io
+- scherf.cool
+- scherf.lol
+- scherf.life
+- scherf.it
+- scherf.zone
+- scherf.email
+- scherf.digital
+- scherf.download
+- scherf.fail
+- scherf.us
+
+# Sections
+
+- Education & experience
+- Hard skills
+- Soft skills
+- Projects
+- Newest commits
+- Contact
+
+## Skills
+
+### Hard Skills
+
+- Python * * * * *
+- JavaScript * * * *
+- HTML5 & CSS * * * *
+- Elm * * * *
+- C# * * *
+- Java * * *
+- Clojure * * *
+
+### Soft Skills
+
+- Communications * * * * * 
+- Writing * * * * *
+- Teamwork * * * * * 
+- Leadership * * * * * 
+- Project Management * * * * *
+- Research * * * * *
+- Negotiation * * * * *
