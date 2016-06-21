9e585ce676d13d830bcbceb069d73f979c3bdc00
rgscherf <rgscherf@gmail.com>
2016-06-20 21:19:33 -0400

    static content and layout additions

--- application/views/home.php
+++ application/views/home.php
@@ -2,59 +2,231 @@
     <head>
         <title>@rgschef cv</title>
         <link rel="stylesheet" href="../css/style.css">
+        <script src="https://use.fontawesome.com/3214b7792e.js"></script>
         <script type="text/javascript" src="../bower_components/jquery/dist/jquery.min.js"></script>
-        <link href="https://fonts.googleapis.com/css?family=Arvo|Codystar|Lato|Monofett|Patua+One|Poiret+One|Simonetta" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Arvo|Bungee+Shade|Lobster+Two|VT323" rel="stylesheet">
     </head>
     <body>
-        <div id="headline">
-            Robert Scherf
-        </div>
-        <div id="subline">
-            Software developer
-        </div>
-        <div id="experience">
-            <div class="sectionHeader">
-                Education & Experience
+        <div id="contentWrapper">
+            <div id="headline">
+                Robert Scherf
             </div>
-            <div class="sectionBody">
-                askghak;gha;k dghsdg
-                dfklgjhsdklfgh;dsfhg <br />
-                askghak;gha;k dghsdg
-                dfklgjhsdklfgh;dsfhg <br />
-                askghak;gha;k dghsdg
-                dfklgjhsdklfgh;dsfhg <br />
+            <div id="subline">
+                I make software.
             </div>
-        </div>
-        <div id=skills>
-            <div class="sectionHeader">
-                Skillset
-            </div>
-            <div class="sectionBody">
-                <div id="skillsHard">
-                    Python * * * * * <br />
-                    JavaScript * * * * <br />
-                    HTML5 & CSS * * * * <br />
-                    Elm * * * * <br />
-                    C# * * * <br />
-                    Java * * * <br />
-                    Clojure * * *  <br />
+            <div id="experience">
+                <div class="sectionHeader">
+                    Education & Experience
                 </div>
-                <div id="skillsSoft">
-                    Communications * * * * *  <br />
-                    Writing * * * * * <br />
-                    Teamwork * * * * *  <br />
-                    Leadership * * * * *  <br />
-                    Project Management * * * * * <br />
-                    Research * * * * * <br />
-                    Negotiation * * * * * <br />
+                <div class="sectionBody" id="experienceCards">
+                    <div class="experienceCard">
+                        <div class="fontAwesomeStack">
+                            <i class="fa fa-graduation-cap fa-2x experienceIcon" aria-hidden="true"></i>
+                        </div>
+                        <div class="experienceDescription">
+                            <div>
+                                2013 - 2015
+                            </div>
+                            <div>
+                                Master of Public Policy
+                            </div>
+                            <div>
+                                Focus in Digital Government
+                            </div>
+                        </div>
+                    </div>
+                    <div class="experienceCard">
+                        <div class="fontAwesomeStack">
+                            <i class="fa fa-university fa-2x experienceIcon" aria-hidden="true"></i>
+                        </div>
+                        <div class="experienceDescription">
+                            <div>
+                                2015 - Present
+                            </div>
+                            <div>
+                                Sr. Communications Consultant
+                            </div>
+                            <div>
+                                Ministry of Municipal Affairs
+                            </div>
+                        </div>
+                    </div>
+                    <div class="experienceCard">
+                        <div class="fontAwesomeStack">
+                            <i class="fa fa-code fa-2x experienceIcon" aria-hidden="true"></i>
+                        </div>
+                        <div class="experienceDescription">
+                            <div>
+                                2016 - Present
+                            </div>
+                            <div>
+                                Freelance developer
+                            </div>
+                            <div>
+                                Toronto, Ontario
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div id="projects">
-        </div>
-        <div id="commits">
-        </div>
-        <div id="contact">
-        </div>
-    </body>
-</html>
+            <div id=skills>
+                <div class="sectionHeader">
+                    Skillset
+                </div>
+                <div class="sectionBody">
+                    <div id="skillsHard">
+                        <table>
+                            <tr>
+                                <td>Python</td>
+                                <td class="skillIcon">
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>JavaScript</td>
+                                <td class="skillIcon">
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>HTML & CSS</td>
+                                <td class="skillIcon">
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>C#</td>
+                                <td class="skillIcon">
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Java</td>
+                                <td class="skillIcon">
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div id="skillsSoft">
+                        <table>
+                            <tr>
+                                <td>Communications</td>
+                                <td class="skillIcon">
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Project Management</td>
+                                <td class="skillIcon">
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Teamwork</td>
+                                <td class="skillIcon">
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Leadership</td>
+                                <td class="skillIcon">
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Writing</td>
+                                <td class="skillIcon">
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                    <i class="fa fa-star fa-3x" aria-hidden="true"></i>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div id="projects">
+                <div class="sectionHeader">
+                    Selected Projects
+                </div>
+            </div>
+            <div id="commits">
+                <div class="sectionHeader">
+                    Latest Commits
+                </div>
+            </div>
+            <div id="contact">
+                <div class="sectionHeader">
+                    Contact
+                </div>
+                <div class="sectionBody" id="contactCards">
+                    <div class="experienceCard">
+                        <div class="fontAwesomeStack">
+                            <i class="fa fa-mobile fa-2x contactIcon" aria-hidden="true"></i>
+                        </div>
+                        <div class="experienceDescription">
+                            416-779-2474
+                        </div>
+                    </div>
+                    <div class="experienceCard">
+                        <div class="fontAwesomeStack">
+                            <i class="fa fa-envelope-o fa-2x contactIcon" aria-hidden="true"></i>
+                        </div>
+                        <div class="experienceDescription">
+                            rgscherf@gmail.com
+                        </div>
+                    </div>
+                    <div class="experienceCard">
+                        <div class="fontAwesomeStack">
+                            <i class="fa fa-twitter fa-2x contactIcon" aria-hidden="true"></i>
+                        </div>
+                        <div class="experienceDescription">
+                            @rgscherf
+                        </div>
+                    </div>
+                    <div class="experienceCard">
+                        <div class="fontAwesomeStack">
+                            <i class="fa fa-github fa-2x contactIcon" aria-hidden="true"></i>
+                        </div>
+                        <div class="experienceDescription">
+                            rgscherf
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </body>
+    </html>
